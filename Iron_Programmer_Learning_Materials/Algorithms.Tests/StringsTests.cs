dd3e4054658227a9bf71b4b60f7702929a14cae8
Iosif Dzeranov <idzeranov@plazius.ru>
2017-11-17 11:02:02 +0300

    Add Levenshtein algorithms with unit tests

---
+++ Iron_Programmer_Learning_Materials/Algorithms.Tests/StringsTests.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Algorithms.Strings;
+using FluentAssertions;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace Algorithms.Tests
+{
+    internal sealed class StringsTests
+    {
+        private Fixture _fixture;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            _fixture = new Fixture();
+        }
+
+        [Test]
+        public void Levenshtein_OneStringEmpty_LengthSecondStringReturned()
+        {
+            // Arrange
+            var s = "";
+            Random r = new Random();
+            var t = string.Join(string.Empty, _fixture.CreateMany<char>(r.Next(100)));
+
+            var expected = new Prescription(t.Length, "");
+
+
+            // Act
+            var levenshtein1 = Strings.Strings.Levenshtein(s, t);
+            var levenshtein2 = Strings.Strings.Levenshtein(t, s);
+
+            // Assert
+            Assert.AreEqual(expected.Distance, levenshtein1.Distance, $"Levenshtein distance should be {expected.Distance}, but it is {levenshtein1.Distance}.");
+            Assert.AreEqual(expected.Distance, levenshtein2.Distance, $"Levenshtein distance should be {expected.Distance}, but it is {levenshtein2.Distance}.");
+        }
+
+        [Test]
+        public void Levenshtein_EqualStrings_ZeroReturned()
+        {
+            // Arrange
+            Random r = new Random();
+            var s = string.Join(string.Empty, _fixture.CreateMany<char>(r.Next(100)));
+            var t = s;
+            var expectedPath = "";
+            for (var i = 0; i < t.Length; i++)
+            {
+                expectedPath += "M";
+            }
+            var expected = new Prescription(0, expectedPath);
+
+
+            // Act
+            var actual1 = Strings.Strings.Levenshtein(s, t);
+            var actual2 = Strings.Strings.Levenshtein(t, s);
+
+            // Assert
+            actual1.ShouldBeEquivalentTo(expected);
+            actual2.ShouldBeEquivalentTo(expected);
+        }
+
+        [Test]
+        public void Levenshtein_Insert_DeficitCharsReturned()
+        {
+            // Arrange
+            var s = "ABC";
+            var t = "ABCDEF";
+            var expected = new Prescription(3, "MMMIII");
+
+            // Act
+            var actual = Strings.Strings.Levenshtein(s, t);
+
+            // Assert
+            actual.ShouldBeEquivalentTo(expected);
+        }
+
+        [Test]
+        public void Levenshtein_DeleteAndInsert_DeficitCharsReturned()
+        {
+            // Arrange
+            var s = "ABC";
+            var t = "BCDE";
+            var expected = new Prescription(3, "MMII");
+
+            // Act
+            var actual = Strings.Strings.Levenshtein(s, t);
+
+            // Assert
+            actual.ShouldBeEquivalentTo(expected);
+        }
+
+
+        [Test]
+        public void Levenshtein_DeleteAndInsert2_DeficitCharsReturned()
+        {
+            // Arrange
+            var s = "BCDE";
+            var t = "ABCDEF";
+            var expected = new Prescription(2, "MMMMI");
+
+            // Act
+            var actual = Strings.Strings.Levenshtein(s, t);
+
+            // Assert
+            actual.ShouldBeEquivalentTo(expected);
+        }
+
+        [Test]
+        public void Levenshtein_ComplexityExample_DeficitCharsReturned()
+        {
+            // Arrange
+            var s = "ATGTTATA";
+            var t = "ATCGTCC";
+            var expected = new Prescription(5, "MMIMMRRDD");
+
+            // Act
+            var actual = Strings.Strings.Levenshtein(s, t);
+
+            // Assert
+            actual.ShouldBeEquivalentTo(expected);
+        }
+    }
+}
---
+++ Iron_Programmer_Learning_Materials/Algorithms/Strings/Strings.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text;
+
+namespace Algorithms.Strings
+{
+    public class Strings
+    {
+        /// <summary>
+        /// Calculates Edit Distance between S1 and S2 and the prescription 
+        /// </summary>
+        /// <param name="s">First input string</param>
+        /// <param name="t">Second input string</param>
+        /// <returns>Prescription - the edit distance and the path retraced</returns>
+        public static Prescription Levenshtein(string s, string t)
+        {
+            int m = s.Length, n = t.Length;
+            var d = new int[m + 1, n + 1];
+            var p = new char[m + 1, n + 1];
+            int i, j;
+            // First fill the basic values
+            for (i = 0; i <= m; i++)
+            {
+                d[i, 0] = i;
+                p[i, 0] = 'D';
+            }
+            for (i = 0; i <= n; i++)
+            {
+                d[0, i] = i;
+                p[0, i] = 'I';
+            }
+
+            for (i = 1; i <= m; i++)
+                for (j = 1; j <= n; j++)
+                {
+                    int cost = s[i - 1] != t[j - 1] ? 1 : 0;
+                    if (cost == 0)
+                    {
+                        d[i, j] = d[i - 1, j - 1];
+                    }
+                    if (d[i, j - 1] < d[i - 1, j] && d[i, j - 1] < d[i - 1, j - 1] + cost)
+                    {
+                        //Inserting
+                        d[i, j] = d[i, j - 1] + 1;
+                        p[i, j] = 'I';
+                    }
+                    else if (d[i - 1, j] < d[i - 1, j - 1] + cost)
+                    {
+                        //Deleting
+                        d[i, j] = d[i - 1, j] + 1;
+                        p[i, j] = 'D';
+                    }
+                    else
+                    {
+                        //Doing nothing or replacing
+                        d[i, j] = d[i - 1, j - 1] + cost;
+                        p[i, j] = cost == 1 ? 'R' : 'M';
+                    }
+                }
+
+            //Retracing
+            var route = new StringBuilder("");
+            i = m;
+            j = n;
+            do
+            {
+                var c = p[i, j];
+                route.Append(c);
+                switch (c)
+                {
+                    case 'R':
+                    case 'M':
+                        i--;
+                        j--;
+                        break;
+                    case 'D':
+                        i--;
+                        break;
+                    default:
+                        j--;
+                        break;
+                }
+            } while (i != 0 && j != 0);
+
+            var charArray = route.ToString().ToCharArray();
+            Array.Reverse(charArray);
+            return new Prescription(d[m, n], new string(charArray));
+        }
+
+        /// <summary>
+        /// Calculates Edit Distance between S1 and S2
+        /// </summary>
+        /// <param name="s">First input string</param>
+        /// <param name="t">Second input string</param>
+        /// <returns></returns>
+        public static int LevenshteinDistance(string s, string t)
+        {
+            return Levenshtein(s, t).Distance;
+        }
+
+        /// <summary>
+        /// Calculates Edit path between S1 and S2 
+        /// </summary>
+        /// <param name="s">First input string</param>
+        /// <param name="t">Second input string</param>
+        /// <returns></returns>
+        public static string LevenshteinPath(string s, string t)
+        {
+            return Levenshtein(s, t).Path;
+        }
+    }
+}
