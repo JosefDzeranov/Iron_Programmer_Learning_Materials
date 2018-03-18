14085659ef4870cf73977e084b2bd55edd8194d2
Iosif Dzeranov <idzeranov@plazius.ru>
2018-03-18 19:52:27 +0300

    add string extensions

--- Iron_Programmer_Learning_Materials/Algorithms/Extensions/StringExtensions.cs
+++ Iron_Programmer_Learning_Materials/Algorithms/Extensions/StringExtensions.cs
@@ -1,7 +1,27 @@
-﻿namespace Algorithms
+﻿using System;
+using System.Net;
+
+namespace Algorithms
 {
     internal static class StringExtensions
     {
+        private delegate bool TryParseDelegate<T>(string s, out T result);
+
+        private static T To<T>(string value, TryParseDelegate<T> parse)
+            => parse(value, out T result) ? result : default;
+
+        public static int ToInt32(this string value)
+            => To<int>(value, int.TryParse);
+
+        public static DateTime ToDateTime(this string value)
+            => To<DateTime>(value, DateTime.TryParse);
+
+        public static IPAddress ToIPAddress(this string value)
+            => To<IPAddress>(value, IPAddress.TryParse);
+
+        public static TimeSpan ToTimeSpan(this string value)
+            => To<TimeSpan>(value, TimeSpan.TryParse);
+
         internal static bool Exist(this string data, char item)
         {
             foreach (var _char in data)
